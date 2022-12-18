04d2d52effbb8a69228f2868c0744addc9bc18f5
Fedor Shakrun <fshakrun@gmail.com>
2022-12-18 15:29:43 +0300

    postman testes succes

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,45 @@
+package ru.netology;
+
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldCheckBody() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Netology Echo Postman") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("Netology Echo Postman"))
+        ;
+    }
+    
+    @Test
+    public void shouldCheckBodyId() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("id:1")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("id:1"))
+        ;
+    }
+}
