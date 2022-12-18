3c9da2d934246dc58e2be02d9cf9573d2de7c0f6
Fedor Shakrun <fshakrun@gmail.com>
2022-12-18 15:38:35 +0300

    postman testes fixed pass

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -39,7 +39,7 @@ public class PostmanEchoTest {
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", equalTo("id:2"))
+                .body("data", equalTo("id:1"))
         ;
     }
 }
